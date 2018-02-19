82c60ce1b31beee099dae3e7c94d7583d1ffc804
Mariano Ruiz <mrsarm@gmail.com>
2018-02-19 09:57:40 -0300

    Added module django-admin-view-permission. Ignore virtual environments in .gitignore
    (cherry picked from master commit 491b81497a9fba58b21d2ac29d9e4cd8a6efecdd)

--- coleman/settings.py
+++ coleman/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [ '*' ]
 INSTALLED_APPS = [
     'mtasks.apps.MtasksConfig',
     'flat_responsive',
+    'admin_view_permission',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
