d6658f1a9e4d4e71b9de87cbfa166b6395b9b7a3
Mariano Ruiz <mrsarm@gmail.com>
2019-04-30 19:49:05 -0300

    Added user serializer to the task serializer

--- mtasks/serializers.py
+++ mtasks/serializers.py
@@ -1,10 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
 from partner.serializers import PartnerSerializer
 from .models import Task
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+        )
+
+
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     partner = PartnerSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Task
@@ -12,6 +24,7 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
             'number',
             'title',
             'partner',
+            'user',
             'description',
             'resolution',
             'deadline',
