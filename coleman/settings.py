18363f6b6a56e0159c0f27d208beccc6c411ee87
Mariano Ruiz <mrsarm@gmail.com>
2022-02-11 19:57:31 -0300

    Add django-extensions and better pin dependencies

--- coleman/settings.py
+++ coleman/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 REST_ENABLED = env.bool('REST_ENABLED', False)
